0d0e9915ed9c95a83e05558b4a77391ab90c5f08
Your Name <you@example.com>
2025-04-28 13:22:48 +0200

    wip bcksp support

--- bfsh.sh
+++ bfsh.sh
@@ -3,30 +3,42 @@
 # from here
 # https://community.hpe.com/t5/operating-system-hp-ux/reading-a-single-character-then-what/td-p/2880639
 
-echo -n W
-echo A
+exit=0
+command_end=1
+while true; do
+	stty -echo
+	stty raw
 
-while echo 1; do
-    stty -echo
-    stty raw
-    readchar=$(dd if=$(tty) bs=1 count=1 2>/dev/null)
+	if [ $command_end -eq 1 ]; then
+		printf '> '
+		command_end=0
+	fi
 
-    case $readchar in
-    $'\003')
-        stty -raw
-        stty sane
-        stty erase "^H"
-        exit 1
-        ;;
-
-    *)
-        stty -raw
-        echo "you typed '$readchar'"
-        echo "$readchar" | head -c1 | xxd
-        ;;
-    esac
-    stty -raw
-    stty sane
-    stty erase "^H"
+	readchar=$(dd if=$(tty) bs=1 count=1 2>/dev/null)
+	# echo -n "$readchar" | xxd
+	# printf "%d" "\"$readchar"
+	if [ $(printf '%02x' "\"$readchar") = "03" ] || [ $(printf '%02x' "\"$readchar") = "04" ]; then
+		exit=1
+	elif [ $(printf '%02x' "\"$readchar") = "0d" ]; then
+		stty -raw
+		command_end=1
+		printf "\n"
+	elif [ $(printf '%02x' "\"$readchar") = "09" ]; then
+		stty -raw
+		stty sane
+		printf "\b \b"
+	else
+		stty -raw
+		printf "$readchar"
+		# echo "$readchar" | head -c1 | xxd
+	fi
+	stty -raw
+	stty sane
+	stty erase "^H"
+	if [ $exit -eq 1 ]; then
+		echo
+		stty sane
+		exit 1
+	fi
 done
 exit 0
