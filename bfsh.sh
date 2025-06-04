52ef4675cd969c6bc03e5bf9b327408528172a9e
Birh Burh <birhburh@gmail.com>
2025-06-04 23:48:38 +0200

    new approach

--- bfsh.sh
+++ bfsh.sh
@@ -1,38 +1,20 @@
-#!/bin/bash
+exec 3<&0 4>&1 5< <( 
+	while true; do
+		IFS= read -rsn4 -u 3 -t1 input
+		status=$?
+		for (( i=0; i<${#input}; i++ )); do
+			printf "%02x" "\"${input:$i:1}"
+		done
+    	done 
+	printf "%02" "\"D"
+	printf "%02" "\"O"
+	printf "%02" "\"N"
+	printf "%02" "\"E"
+) 
 
-# stty dd variant for sh version (unfortunatly not pure version
-# https://community.hpe.com/t5/operating-system-hp-ux/reading-a-single-character-then-what/td-p/2880639
-
-exit=0
-input=""
-command_end=1
 while true; do
-	if [ $command_end -eq 1 ]; then
-		printf '> '
-		command_end=0
-	fi
-
-	read -rsn1 binput &
-	read -rsn1 input
-	hexchar=$(printf "%02x" "\"$input") 
-	printf "|$hexchar|: "
-	hexchar=$(printf "%02x" "\"$binput") 
-	printf "|$hexchar|: "
-	# printf "|$hexchar|: "
-	continue
-	if [ $hexchar = "03" ] || [ $hexchar = "04" ]; then
-		exit=1
-	elif [ $hexchar = "0d" ]; then
-		command_end=1
-		printf "\n"
-	elif [ $hexchar = "7f" ]; then
-		printf "\b\033[0J"
-	else
-		printf "$readchar"
-	fi
-	if [ $exit -eq 1 ]; then
-		echo
-		exit 1
-	fi
+    IFS= read -rsn2 -u5 binput
+    # printf "$binput"
+    printf "\x$binput"
 done
-exit 0
+
